437a5d62e1e1812ab1c6679d5d0c8064efdbd7ed
Kotapothula Anil Kumar <113521220+Anilkumar2605@users.noreply.github.com>
2023-12-12 12:02:00 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello{
     public static void main(String arg[]){
 
         System.out.println("Hello Git....");
-        System.out.println("Iam Anil");
         String d = "26";
         String m = "04";
         String y = "2002";
